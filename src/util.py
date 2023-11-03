dc30a5b42a6e7d507ebcd3ce53bcb3406989d855
Reetesh Sudhakar <rsudhakar9@gatech.edu>
2023-11-03 18:23:35 -0400

    add PCA process function to util file

--- src/util.py
+++ src/util.py
@@ -1,3 +1,7 @@
+import pandas as pd 
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
 ignore_columns = ['APARTMENTS_AVG', 'BASEMENTAREA_AVG', 'YEARS_BEGINEXPLUATATION_AVG',
                   'YEARS_BUILD_AVG', 'COMMONAREA_AVG', 'ELEVATORS_AVG', 'ENTRANCES_AVG',
                   'FLOORSMAX_AVG', 'FLOORSMIN_AVG', 'LANDAREA_AVG', 'LIVINGAPARTMENTS_AVG',
@@ -15,9 +19,20 @@ ignore_columns = ['APARTMENTS_AVG', 'BASEMENTAREA_AVG', 'YEARS_BEGINEXPLUATATION
                   'FLAG_DOCUMENT_12', 'FLAG_DOCUMENT_13', 'FLAG_DOCUMENT_14', 'FLAG_DOCUMENT_15', 'FLAG_DOCUMENT_16',
                   'FLAG_DOCUMENT_17', 'FLAG_DOCUMENT_18', 'FLAG_DOCUMENT_19', 'FLAG_DOCUMENT_20', 'FLAG_DOCUMENT_21']
 
-def defaultClean(df):
+def defaultClean(df: pd.DataFrame) -> None:
     df.drop(ignore_columns, axis=1, inplace=True)
 
-def dropColumnsBetween(df, start, end):
+def dropColumnsBetween(df: pd.DataFrame, start: str, end: str) -> None:
     columns = df.columns.tolist()
     df.drop(columns[columns.index(start) : columns.index(end) + 1], axis=1, inplace=True)
+
+def run_pca(df: pd.DataFrame, n_components: int) -> pd.DataFrame:
+    # Standardize the data
+    X_std = StandardScaler().fit_transform(df)
+
+    # running PCA
+    pca = PCA(n_components=n_components)
+    principal_components = pca.fit_transform(X_std)
+    principal_df = pd.DataFrame(data=principal_components, columns = [f'Component {x}' for x in range(1, n_components + 1)])
+
+    return principal_df
